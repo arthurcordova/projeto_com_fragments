10950b62a83abbc0b8000155aa6b94c3242fc66c
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-08-10 14:59:15 -0300

    Fragments
    Fragments manager
    
    Co-Authored-By: Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>

--- app/src/main/java/com/proway/projetofragments/MainActivity.kt
+++ app/src/main/java/com/proway/projetofragments/MainActivity.kt
@@ -2,20 +2,42 @@ package com.proway.projetofragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.proway.projetofragments.fragments.PrimeiroFragment
 import com.proway.projetofragments.fragments.SegundoFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var tipoFragment = TipoFragment.PRIMEIRO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container1, PrimeiroFragment())
-            .commitNow()
 
+        findViewById<Button>(R.id.buttonFrag1).apply {
+            setOnClickListener {
+                if (tipoFragment == TipoFragment.PRIMEIRO) {
+                    replaceFrag(PrimeiroFragment())
+                    tipoFragment = TipoFragment.SEGUNDO
+                } else if (tipoFragment == TipoFragment.SEGUNDO) {
+                    replaceFrag(SegundoFragment())
+                    tipoFragment = TipoFragment.PRIMEIRO
+                }
+            }
+        }
+
+
+    }
+
+    private fun replaceFrag(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container2, SegundoFragment())
+            .replace(R.id.container, fragment)
             .commitNow()
     }
+}
+
+enum class TipoFragment {
+    PRIMEIRO,
+    SEGUNDO
 }
